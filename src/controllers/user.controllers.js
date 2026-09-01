810592f697e331a4eff74570e88023f122202864
IbraheemMudasser <ibraheemhot10@gmail.com>
2026-09-01 09:42:06 -0700

    set Up the register function and fix small bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,58 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError from '../utils/errorHandler.js'
+import {User} from '../models/user.model.js'
+import {uploadOnCloude} from '../utils/cloudinary.js'
+import { ApiRespons } from "../utils/apiResponse.js";
+
 
 const registerUser = asyncHandler(async(req, res)=>{
-    console.log('runn controller')
-   return res.status(200).json({
-        message: 'Post Request work successfully'
+    const {userName , email , fullName , password} = req.body
+
+    if ([userName, email, fullName, password]
+    .some(field => !field?.trim())) {
+        throw new ApiError(400 , 'All fields are required');
+    }
+
+    const isUserExist = await User.findOne({
+        $or:[{userName} , {email}]
     })
+
+    if (isUserExist) {
+        throw new ApiError(409 , 'User Already Exist')
+    }
+    
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    
+    if (!avatarLocalPath) {
+        throw new ApiError(400 , 'Avatar file is required')
+    }
+    
+    const avatarUploadResult = await uploadOnCloude(avatarLocalPath)
+    const coverImageUploadResult = await uploadOnCloude(coverImageLocalPath)
+    
+    if (avatarUploadResult) {
+        throw new ApiError(400 , 'Avatar file is required')
+    }
+
+    const createUserResult= await User.create({
+        fullName,
+        email,
+        password,
+        userName: userName?.toLowerCase(),
+        avatar: avatarLocalPath?.url, 
+        coverImage: coverImageLocalPath?.url || "", 
+    })
+
+    const checkIsUserCreated = await User.findById(createUserResult?._id).select("-password -refreshToken")
+
+    if (!checkIsUserCreated) {
+        throw new ApiError('500' , 'User Not Created')
+    }
+    
+    return res.status(200).json(
+        new ApiRespons(201 , checkIsUserCreated , 'User Created Successfully')
+    )
 })
 
 export {registerUser}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,3 @@
 export const asyncHandler =(requestHandler)=>{ return (req, res, next)=>{
-    Promise.resolve(requestHandler).catch((err)=>next(err))
+    Promise.resolve(requestHandler(req, res , next)).catch((err)=>next(err))
 }}
